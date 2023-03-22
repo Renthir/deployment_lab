e51b37cb6f9be9737d1ebfec058e875f91d28d0a
Seth Reid <reid.seth.96@gmail.com>
2023-03-22 16:16:04 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+app.use(express.static(`${__dirname}/public`))
+
+
+
+app.listen(4000, () => console.log('App up on 4000'))
